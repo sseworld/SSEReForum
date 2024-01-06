53e3689061adfcbdc62675e5c3faf96fdadad2ff
Shashikant Maurya <114757626+ssseworld@users.noreply.github.com>
2024-01-06 20:58:33 +0530

    Initial commit

---
+++ config/credential.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GITHUB_CLIENT_ID: "ac0d2ebafa3baec47614",
+  GITHUB_CLIENT_SECRET: "106c4c581951b827936a1febbaa4c28286db61ae",
+  GITHUB_CALLBACK_URL: "http://localhost:5173/",
+  DBURL: "mongodb://localhost:27017/ST",
+};
---
+++ frontend/Components/Dashboard/index.jsx
@@ -0,0 +1,4 @@
+import Counts from "./Counts";
+import ForumBox from "./ForumBox";
+
+export { Counts, ForumBox };
